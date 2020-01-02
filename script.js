98993488bb2fc0dba6274fff943ff5a97c4eaef4
samietheturtle <mari219x@stud.kea.dk>
2020-01-02 22:26:50 +0100

    burger menu done

---
+++ script.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+    const navLinks = document.querySelectorAll('.nav_links li');
+
+
+    burger.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav_active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+            }
+        });
+
+        //Burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
